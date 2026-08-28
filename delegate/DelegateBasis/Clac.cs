6adf06f730e2750de6f486bbb76814f85e43144d
mohamedSayedBayoumy <mandobobo12345@gmail.com>
2026-08-28 19:15:49 +0300

    Delegate Basic

---
+++ delegate/DelegateBasis/Clac.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegateAvanced.DelegateBasis
+{
+
+    public delegate int MathOperation(int a, int b);
+    internal class Clac
+    {
+        static public int Add(int a, int b) => a + b;
+        static public int Multiply(int a, int b)
+        {
+            Console.WriteLine("Multiply");
+            return a * b;
+        }
+
+        static public double Subtract(double a, double b) => a - b;
+
+    }
+}
--- delegate/Program.cs
+++ delegate/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Delegate
+﻿using DelegateAvanced.DelegateBasis;
+
+namespace Delegate
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Delegate Basic
+            //MathOperation mathOperation = Clac.Add;
+
+            //var resultWithInvoke = mathOperation.Invoke(10, 20);
+            //var result = mathOperation(10, 20);
+            //Console.WriteLine($"Result with Invoke: {resultWithInvoke}");
+            //Console.WriteLine($"Result With Syntax Sugar: {result}");
+
+            //Console.WriteLine("=======================================");
+
+            //mathOperation = Clac.Multiply;
+            //var resultMultiply = mathOperation.Invoke(10, 20);
+            //Console.WriteLine($"Result Multiply: {resultMultiply}");
+
+
+            //// mathOperation = Clac.Subtract; //  Here will give error Cause Subtract Fun not Match the Delegate Singnature
+            #endregion  
+
+
         }
     }
 }
